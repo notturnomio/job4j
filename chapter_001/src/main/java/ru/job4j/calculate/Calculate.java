bc2da429fd3e0d61335e8b1f751dcaa38b08f678
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-06-19 22:16:05 +0600

    1.3.JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,17 @@
 package ru.job4j.calculate;
-
+/**
+ * Calculate.
+ *
+ * @author Rustem Mukazhanov (r.mukazhanov@gmail.com)
+ * @since 0.1
+ * @version $Id$
+ */
 public class Calculate {
-
+/**
+    * Main.
+    * @param args - args.
+    */
     public static void main (String[] args) {
     	System.out.println("Hello World!");
     }
-   }
+}
