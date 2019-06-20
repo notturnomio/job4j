a5fb1b69e07774d5c301a228e7f30a6e6c9cca99
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-06-20 22:47:08 +0600

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ * 
+ * @author Rustem Mukazhanov (r.mukazhanov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+    /**
+    * Test echo.
+    */
+    
+@Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+    	String input = "Rustem Mukazhanov";
+    	String expect = "Echo, echo, echo : Rustem Mukazhanov";
+    	Calculate calc = new Calculate();
+    	String result  = calc.echo(input);
+    	assertThat(result, is(expect));
+    }
+}
