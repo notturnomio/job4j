d0ad97424833fa281cf64a0793e48514875110f7
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-06-21 22:42:35 +0600

    2.5. Инспектор кода IDEA. [#69757]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,24 +1,28 @@
 package ru.job4j.calculate;
+
 /**
  * Calculate.
  *
  * @author Rustem Mukazhanov (r.mukazhanov@gmail.com)
- * @since 0.1
  * @version $Id$
+ * @since 0.1
  */
 public class Calculate {
-/**
-    * Main.
-    * @param args - args.
-    */
-    public static void main(String[] args) {
-    	System.out.println("Hello World!");
-    }
     /**
-    * Method echo.
-    * @param name Your name.
-    * @return Echo plus your name.
-    */
+     * Main.
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
     public String echo(String name) {
         return "Echo, echo, echo : " + name;
     }
