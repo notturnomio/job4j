ae7b872df7a18903024d70694a55213b215955da
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-07-23 00:26:32 +0600

    5.2. Создать программу, вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/loop/Factorial.java
@@ -0,0 +1,24 @@
+package loop;
+
+/**
+ * The program that calculates a factorial with for loop.
+ *
+ * @author Rustem Mukazhanov (r.mukazhanov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Factorial {
+    /**
+     * Finding factorial.
+     *
+     * @param n - factorial value.
+     * @return - factorial result.
+     */
+    public int calc(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/loop/FactorialTest.java
@@ -0,0 +1,40 @@
+package loop;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * The test that checks a factorial with for loop.
+ *
+ * @author Rustem Mukazhanov (r.mukazhanov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class FactorialTest {
+
+    /**
+     * Test n = 5.
+     */
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        Factorial factorial = new Factorial();
+        int result = factorial.calc(5);
+        int expected = 120;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test n = 0.
+     */
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        Factorial factorial = new Factorial();
+        int result = factorial.calc(0);
+        int expected = 1;
+        assertThat(result, is(expected));
+    }
+}
