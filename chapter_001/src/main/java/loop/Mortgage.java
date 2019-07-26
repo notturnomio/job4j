0dc51a5b731d69121935f7d382da7635333da61b
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-07-26 21:27:48 +0600

    5.6. Ипотека. [#156315]

---
+++ chapter_001/src/main/java/loop/Mortgage.java
@@ -0,0 +1,26 @@
+package loop;
+
+/**
+ * Mortage program calculates number of years needed to pay a credit.
+ */
+public class Mortgage {
+    /**
+     * Method year calculates number of years.
+     *
+     * @param amount  of credit.
+     * @param payment payed monthly.
+     * @param percent yearly
+     * @return number of years.
+     */
+    public int year(int amount, int payment, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            int interest = (int) (amount * percent / 100);
+            int annual = payment * 12;
+            amount = (amount + interest) - annual;
+            year++;
+        }
+        return year;
+    }
+}
+
---
+++ chapter_001/src/test/java/loop/MortgageTest.java
@@ -0,0 +1,28 @@
+package loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MortgageTest {
+    /**
+     * Testing the number of years when payment is 100 with 1%.
+     */
+    @Test
+    public void when1Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(1000, 100, 1);
+        assertThat(year, is(1));
+    }
+
+    /**
+     * Testing the number of years when payment is 10 with 50%.
+     */
+    @Test
+    public void when2Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(100, 10, 50);
+        assertThat(year, is(2));
+    }
+}
