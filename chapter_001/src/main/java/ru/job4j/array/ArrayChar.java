8ebc193de18db2d1d4f363908af976462d942127
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-08-14 21:48:09 +0600

    6.4. Слово начинается с ... [#41585]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * Checks if word starts with prefix.
+ *
+ * @author Rustem Mukazhanov (r.mukazhanov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayChar {
+    /**
+     * Method checks if prefix equals to words first letters.
+     *
+     * @param word   given word to check.
+     * @param prefix first letters.
+     * @return result.
+     */
+    public boolean StartsWith(String word, String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+        for (int i = 0; i < prefix.length(); i++) {
+            if (wrd[i] != pref[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayCharTest {
+    /**
+     * Testing if word starts with Hel.
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar();
+        boolean result = word.StartsWith("Hello", "Hel");
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Testing if word starts with Hil.
+     */
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar();
+        boolean result = word.StartsWith("Hello", "Hil");
+        assertThat(result, is(false));
+    }
+}
