8fef159ce1bad104a870d8a6e0eeeca16bdcf091
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-09-03 21:58:08 +0600

    6.5. Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -26,4 +26,25 @@ public class FindLoop {
         }
         return rst;
     }
+
+    /**
+     * Method returns element's index inside of given range.
+     *
+     * @param data   array size.
+     * @param el     element to find.
+     * @param start  first index of range.
+     * @param finish last index of range.
+     * @return element's index.
+     */
+    public int indexOfRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
 }
+
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -31,4 +31,34 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    /**
+     * Testing if 7 is in array between 3 and 5.
+     */
+    @Test
+    public void whenArrayHas7Between3And5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{6, 12, 4, 2, 7, 9, 10};
+        int value = 7;
+        int start = 3;
+        int finish = 5;
+        int result = find.indexOfRange(input, value, start, finish);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Testing if 6 is in array outside of 4 and 7 range.
+     */
+    @Test
+    public void whenArrayHas6Outside4And8ThenMinus1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{6, 14, 3, 8, 11, 5, 2, 0};
+        int value = 6;
+        int start = 4;
+        int finish = 7;
+        int result = find.indexOfRange(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
