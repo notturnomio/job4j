ad02ba4af7b1392cc91856f575a9ae80c24ed7f3
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2019-12-14 18:10:36 +0600

    1.5. Поля объекта [#166083]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -2,6 +2,26 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    private String food;
+
+    private String name;
+
+    public void call() {
+        System.out.println(this.name);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         return "miao-miao";
     }
@@ -10,5 +30,23 @@ public class Cat {
         Cat liza = new Cat();
         String say = liza.sound();
         System.out.println("Liza says: " + say);
+        System.out.println();
+
+        System.out.print("Here is the food of ");
+        Cat gav = new Cat();
+        gav.giveNick("Kotyonok Gav");
+        gav.call();
+        gav.eat("kotleta");
+        gav.show();
+
+        System.out.println();
+
+        System.out.print("Here is the food of ");
+        Cat blackie = new Cat();
+        blackie.giveNick("Chyorniy Kot");
+        blackie.call();
+        blackie.eat("fish");
+        blackie.show();
+
     }
 }
