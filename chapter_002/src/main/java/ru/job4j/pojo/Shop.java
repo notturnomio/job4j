40c4969f4519b2b8a64689e4b715441ffc069045
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2020-05-26 23:47:45 +0600

    3. Удаление моделей из массива. [#176470]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,67 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+
+    public static void main(String[] args) {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем в ячейку 1 значение ячейки 2 и удаляем значение из ячейки 2");
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Смещаем все элементы на одну позицию влево, чтобы в массиве не было дыр");
+        Shop shop = new Shop();
+        products = shop.delete(products, 0);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+}
