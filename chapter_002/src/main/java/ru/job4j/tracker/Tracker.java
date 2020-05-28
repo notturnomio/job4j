c234b4a63155c6248e4342c4439e2ceb3ba5731a
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2020-05-28 23:34:15 +0600

    5. Tracker - хранилище [#396]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,63 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     *
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, position);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] find = new Item[position];
+        int same = 0;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getName().equals(key)) {
+                find[same++] = items[index];
+            }
+        }
+        return Arrays.copyOf(find, same);
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                result = items[index];
+                break;
+            }
+        }
+        return result;
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenFindAll() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("One");
+        Item item2 = new Item("Two");
+        Item item3 = new Item("Three");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = {item1, item2, item3};
+        Item[] result = tracker.findAll();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenFindByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Three");
+        Item item2 = new Item("Two");
+        Item item3 = new Item("Three");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = {item1, item3};
+        Item[] result = tracker.findByName("Three");
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("one");
+        Item item2 = new Item("two");
+        Item item3 = new Item("one");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item result = tracker.findById(item2.getId());
+        assertThat(result, is(item2));
+    }
+
+    @Test
+    public void whenFindByIdNotFound() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("one");
+        Item item2 = new Item("two");
+        Item item3 = new Item("three");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item result = tracker.findById("1");
+        assertThat(result, is(nullValue()));
+    }
+
+}
